8294edbaea13c5b4609c359cb8d28b27b7889514
Paweł <pmiskiew@mion.elka.pw.edu.pl>
2019-02-26 21:29:28 +0100

    initial commit

---
+++ src/components/Landing/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class LandingPage extends React.PureComponent {
+    render() {
+        return (
+            <div>Hello</div>
+        );
+    }
+}
+
+export default LandingPage;
