3d6f728bacfbbeb71172e6d3b81877e1c8c673a7
kompanpawel <pawelmiskiewicz14@gmail.com>
2019-10-10 20:03:12 +0200

    material UI implementation

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,42 +1,72 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import * as ROUTES from "constants/routes";
 import "./Navigation.scss";
 
 import SignOutButton from "components/SignOutButton";
 import { AuthUserContext } from "components/Session";
+import { AppBar, Button, makeStyles, Toolbar } from "@material-ui/core";
+
+const useStyles = makeStyles((theme: any) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 const Navigation: React.FC<any> = () => {
   return (
     <div>
       <AuthUserContext.Consumer>
-        {(authUser) => (authUser ? <NavigationAuth /> : <NavigationNonAuth />)}
+        {(authUser) => (authUser ? <WrappedNavigationAuth /> : <WrappedNavigationNonAuth />)}
       </AuthUserContext.Consumer>
     </div>
   );
 };
 
-const NavigationAuth = () => (
-  <div className="header__navigation">
-    <Link to={ROUTES.MAIN_PAGE} className="header__item">
-      Main Page
-    </Link>
-    <Link to={ROUTES.ACCOUNT} className="header__item">
-      Account
-    </Link>
-    <SignOutButton />
-  </div>
-);
+const NavigationAuth: React.FC<any> = ({ history }) => {
+  const classes = useStyles();
+  const clickHandler = (route: string) => {
+    history.push(route);
+  };
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Button onClick={() => clickHandler(ROUTES.MAIN_PAGE)}>Main Page</Button>
+          <Button onClick={() => clickHandler(ROUTES.ACCOUNT)}>Account</Button>
+          <Button onClick={() => clickHandler(ROUTES.ADMIN)}>Admin</Button>
+          <SignOutButton />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
 
-const NavigationNonAuth = () => (
-  <div className="header__navigation">
-    <Link to={ROUTES.MAIN_PAGE} className="header__item">
-      Main Page
-    </Link>
-    <Link to={ROUTES.SIGN_IN} className="header__item">
-      Sign In
-    </Link>
-  </div>
-);
+const NavigationNonAuth: React.FC<any> = ({ history }) => {
+  const classes = useStyles();
+
+  const clickHandler = (route: string) => {
+    history.push(route);
+  };
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Button onClick={() => clickHandler(ROUTES.MAIN_PAGE)}>Main Page</Button>
+          <Button onClick={() => clickHandler(ROUTES.SIGN_IN)}>Sign In</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+const WrappedNavigationNonAuth = withRouter(NavigationNonAuth);
+const WrappedNavigationAuth = withRouter(NavigationAuth);
 
 export default Navigation;
